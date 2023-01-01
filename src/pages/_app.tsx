0bc3eac8d2b07f67d944475c43128f852b21e1fe
karintou8710 <karintou8710@gmail.com>
2023-01-02 02:21:58 +0900

    [fix] add emotion, CssBaseline

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
 const theme = createTheme({
   palette: {
@@ -12,6 +13,7 @@ const theme = createTheme({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Component {...pageProps} />;
     </ThemeProvider>
   );
