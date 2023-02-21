8fc024557f66c48b44da3e46354e1bc590ede37c
karintou8710 <karintou8710@gmail.com>
2023-02-22 05:35:00 +0900

    [add] header active style

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,16 +1,19 @@
 import { Star } from '@mui/icons-material'
 import SearchIcon from '@mui/icons-material/Search'
 import { AppBar, Toolbar, Typography, Box } from '@mui/material'
+import Image from 'next/image'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { FC } from 'react'
 
 import LinkWithIcon from 'components/molecules/LinkWithIcon'
 import { BaseProps } from 'types'
-import Image from 'next/image'
 
 type HeaderProps = BaseProps
 
 const Header: FC<HeaderProps> = () => {
+  const router = useRouter()
+
   return (
     <AppBar position="static">
       <Toolbar>
@@ -33,10 +36,32 @@ const Header: FC<HeaderProps> = () => {
           </Link>
         </Box>
 
-        <LinkWithIcon href="/search" icon={<SearchIcon />} sx={{ mr: '1rem' }}>
+        <LinkWithIcon
+          href="/search"
+          icon={<SearchIcon />}
+          sx={{
+            mr: '1rem',
+            opacity: 0.7,
+            fontWeight: 600,
+            ...(router.pathname === '/search' && {
+              opacity: 1,
+            }),
+          }}
+        >
           SEARCH
         </LinkWithIcon>
-        <LinkWithIcon href="/bookmark" icon={<Star />} sx={{ mr: '1rem' }}>
+        <LinkWithIcon
+          href="/bookmark"
+          icon={<Star />}
+          sx={{
+            mr: '1rem',
+            opacity: 0.7,
+            fontWeight: 600,
+            ...(router.pathname === '/bookmark' && {
+              opacity: 1,
+            }),
+          }}
+        >
           BOOKMARK
         </LinkWithIcon>
       </Toolbar>
