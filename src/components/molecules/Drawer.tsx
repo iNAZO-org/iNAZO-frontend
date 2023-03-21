61d9be3d36425d4a0deb8ec47d27ae57a435fdbb
karintou8710 <karintou8710@gmail.com>
2023-03-21 16:07:58 +0900

    [add] Drawer component

---
+++ src/components/molecules/Drawer.tsx
@@ -0,0 +1,109 @@
+import { Link as LinkIcon, Star, Home } from '@mui/icons-material'
+import SearchIcon from '@mui/icons-material/Search'
+import {
+  List,
+  SwipeableDrawer,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  ListSubheader,
+} from '@mui/material'
+import { styled } from '@mui/material/styles'
+import Link from 'next/link'
+
+type DrawerRightProps = {
+  open: boolean
+  toggleDrawer: (
+    open: boolean,
+  ) => (event: React.KeyboardEvent | React.MouseEvent) => void
+}
+
+const drawerWidth = 240
+
+export default function DrawerRight({ open, toggleDrawer }: DrawerRightProps) {
+  return (
+    <SwipeableDrawer
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        '& .MuiDrawer-paper': {
+          width: drawerWidth,
+        },
+      }}
+      variant="persistent"
+      anchor="right"
+      onClose={toggleDrawer(false)}
+      onOpen={toggleDrawer(true)}
+      open={open}
+    >
+      <List
+        subheader={
+          <ListSubheader component="div">サイト内リンク</ListSubheader>
+        }
+      >
+        <StyledLink href="/">
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <Home />
+              </ListItemIcon>
+
+              <StyledListItemText primary="Home" />
+            </ListItemButton>
+          </ListItem>
+        </StyledLink>
+
+        <StyledLink href="/search">
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <SearchIcon />
+              </ListItemIcon>
+
+              <StyledListItemText primary="Search" />
+            </ListItemButton>
+          </ListItem>
+        </StyledLink>
+
+        <StyledLink passHref href="/bookmark">
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <Star />
+              </ListItemIcon>
+              <StyledListItemText primary="Bookmark" />
+            </ListItemButton>
+          </ListItem>
+        </StyledLink>
+      </List>
+
+      <List
+        subheader={<ListSubheader component="div">外部リンク</ListSubheader>}
+      >
+        <ListItem
+          disablePadding
+          component="a"
+          href="https://hu-jagajaga.com/"
+          target="_blank"
+        >
+          <ListItemButton>
+            <ListItemIcon>
+              <LinkIcon />
+            </ListItemIcon>
+
+            <StyledListItemText primary="JagaJaga" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </SwipeableDrawer>
+  )
+}
+
+const StyledListItemText = styled(ListItemText)({
+  color: 'black',
+})
+
+const StyledLink = styled(Link)({
+  textDecoration: 'none',
+})
--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,12 +1,14 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 
 import { Star } from '@mui/icons-material'
+import MenuIcon from '@mui/icons-material/Menu'
 import SearchIcon from '@mui/icons-material/Search'
-import { AppBar, Toolbar, Box } from '@mui/material'
+import { AppBar, Toolbar, Box, IconButton, Backdrop } from '@mui/material'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
+import DrawerRight from '@/components/molecules/Drawer'
 import LinkWithIcon from '@/components/molecules/LinkWithIcon'
 import { BaseProps } from '@/types'
 
@@ -14,67 +16,100 @@ type HeaderProps = BaseProps
 
 const Header: FC<HeaderProps> = () => {
   const router = useRouter()
+  const [open, setOpen] = useState(false)
+
+  const toggleDrawer =
+    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event &&
+        event.type === 'keydown' &&
+        ((event as React.KeyboardEvent).key === 'Tab' ||
+          (event as React.KeyboardEvent).key === 'Shift')
+      ) {
+        return
+      }
+
+      setOpen(open)
+    }
 
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Box
-          sx={{
-            flexGrow: 1,
-            ml: '0.5rem',
-            '&:hover': {
-              opacity: 0.7,
-            },
-          }}
-        >
-          <Link href="/">
-            <Image
-              src="/iNAZO_logo.png"
-              alt="iNAZO logo"
-              width={150}
-              height={45}
-            />
-          </Link>
-        </Box>
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Box
+            sx={{
+              flexGrow: 1,
+              ml: '0.5rem',
+              '&:hover': {
+                opacity: 0.7,
+              },
+            }}
+          >
+            <Link href="/">
+              <Image
+                src="/iNAZO_logo.png"
+                alt="iNAZO logo"
+                width={150}
+                height={45}
+              />
+            </Link>
+          </Box>
 
-        <LinkWithIcon
-          href="/search"
-          icon={<SearchIcon />}
-          sx={{
-            mr: '1rem',
-            opacity: 0.7,
-            fontWeight: 600,
-            display: {
-              xs: 'none',
-              sm: 'flex',
-            },
-            ...(router.pathname === '/search' && {
-              opacity: 1,
-            }),
-          }}
-        >
-          SEARCH
-        </LinkWithIcon>
-        <LinkWithIcon
-          href="/bookmark"
-          icon={<Star />}
-          sx={{
-            mr: '1rem',
-            opacity: 0.7,
-            fontWeight: 600,
-            display: {
-              xs: 'none',
-              sm: 'flex',
-            },
-            ...(router.pathname === '/bookmark' && {
-              opacity: 1,
-            }),
-          }}
-        >
-          BOOKMARK
-        </LinkWithIcon>
-      </Toolbar>
-    </AppBar>
+          <LinkWithIcon
+            href="/search"
+            icon={<SearchIcon />}
+            sx={{
+              mr: '1rem',
+              opacity: 0.7,
+              fontWeight: 600,
+              display: {
+                xs: 'none',
+                sm: 'flex',
+              },
+              ...(router.pathname === '/search' && {
+                opacity: 1,
+              }),
+            }}
+          >
+            SEARCH
+          </LinkWithIcon>
+          <LinkWithIcon
+            href="/bookmark"
+            icon={<Star />}
+            sx={{
+              mr: '1rem',
+              opacity: 0.7,
+              fontWeight: 600,
+              display: {
+                xs: 'none',
+                sm: 'flex',
+              },
+              ...(router.pathname === '/bookmark' && {
+                opacity: 1,
+              }),
+            }}
+          >
+            BOOKMARK
+          </LinkWithIcon>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="end"
+            onClick={toggleDrawer(true)}
+            sx={{ ...(open && { display: 'none' }), display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <DrawerRight open={open} toggleDrawer={toggleDrawer} />
+      <Backdrop
+        sx={{ color: '#fff' }}
+        open={open}
+        onClick={toggleDrawer(false)}
+        onKeyDown={toggleDrawer(false)}
+      />
+    </>
   )
 }
 
